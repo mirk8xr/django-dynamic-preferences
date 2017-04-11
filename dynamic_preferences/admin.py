d17da888639e631265da9059e6af028dfffab82a
mirk8xr <mirk8xr@gmail.com>
2017-04-11 11:34:22 +0200

    Fix delete permission

--- dynamic_preferences/admin.py
+++ dynamic_preferences/admin.py
@@ -53,7 +53,7 @@ class DynamicPreferenceAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
-    def has_delete_permission(self, request):
+    def has_delete_permission(self, request, obj=None):
         return False
 
     def get_actions(self, request):
