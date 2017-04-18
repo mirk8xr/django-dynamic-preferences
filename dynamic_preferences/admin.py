f6e3ab831a248073eb6a5da5df210db9a08c8159
mirk8xr <mirk8xr@gmail.com>
2017-04-18 11:38:02 +0200

    Fix form multipart-encoded

--- dynamic_preferences/admin.py
+++ dynamic_preferences/admin.py
@@ -4,6 +4,7 @@
 from django.contrib import admin
 from dynamic_preferences.models import GlobalPreferenceModel, UserPreferenceModel
 from django import forms
+from dynamic_preferences.types import FilePreference
 
 
 class PreferenceChangeListForm(forms.ModelForm):
@@ -16,22 +17,26 @@ class PreferenceChangeListForm(forms.ModelForm):
 
     def is_multipart(self):
         """
-        TODO: capire come fixare meglio l'accrocchio del multipart-encoded
         Returns True if the form needs to be multipart-encoded, i.e. it has
         FileInput. Otherwise, False.
         """
-        return True
+        for section in self.instance.registry:
+            for pref in self.instance.registry[section]:
+                if isinstance(self.instance.registry[section][pref], FilePreference):
+                    return True
+        return False
 
     def __init__(self, *args, **kwargs):
         self.instance = kwargs.get('instance')
-        # self.raw_value = self.instance.preference.setup_field()
-        # self.base_fields = {'raw_value': self.instance.preference.setup_field()}
-        # self.declared_fields = self.base_fields
         super(PreferenceChangeListForm, self).__init__(*args, **kwargs)
         self.fields['raw_value'] = self.instance.preference.setup_field()
 
     def save(self, *args, **kwargs):
-        self.cleaned_data['raw_value'] = self.instance.preference.serializer.serialize(self.cleaned_data['raw_value'])
+        ikargs = {}
+        if isinstance(self.instance.preference, FilePreference):
+            ikargs['delete_filename'] = self.initial['raw_value']
+        self.cleaned_data['raw_value'] = self.instance.preference.serializer.serialize(self.cleaned_data['raw_value'],
+                                                                                       **ikargs)
         return super(PreferenceChangeListForm, self).save(*args, **kwargs)
 
 
@@ -50,7 +55,7 @@ class DynamicPreferenceAdmin(admin.ModelAdmin):
     readonly_fields = ('section', 'name', 'value', 'help')
     fields = ("raw_value",)
     list_display = ['section', 'name', 'raw_value', 'help']
-    list_display_links = ['name',]
+    list_display_links = ['name', ]
     list_editable = ('raw_value',)
     #search_fields = ['section', 'name', 'help']
     search_fields = []
@@ -90,7 +95,8 @@ class UserPreferenceAdmin(DynamicPreferenceAdmin):
     form = UserPreferenceChangeListForm
     changelist_form = UserPreferenceChangeListForm
     list_display = ['user'] + DynamicPreferenceAdmin.list_display
-    search_fields = ['user__username'] + DynamicPreferenceAdmin.search_fields
+    #search_fields = ['user__username'] + DynamicPreferenceAdmin.search_fields
+    search_fields = []
 
 
 admin.site.register(UserPreferenceModel, UserPreferenceAdmin)
