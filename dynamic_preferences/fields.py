456df7a1d0d36e8cf3d613438e9cd76a241acc1e
fconte <ciccio_79@yahoo.it>
2019-01-30 11:56:34 +0100

    fix FieldUpfile url

--- dynamic_preferences/fields.py
+++ dynamic_preferences/fields.py
@@ -4,6 +4,8 @@
 import os
 import urlparse
 from django.conf import settings
+from django.utils.encoding import filepath_to_uri
+
 from dynamic_preferences.settings import preferences_settings
 from django.db.models.fields.files import FieldFile
 
@@ -17,4 +19,4 @@ class FieldUpfile(FieldFile):
 
     @property
     def url(self):
-        return self.get_file_url(self.name)
+        return self.get_file_url(filepath_to_uri(self.name))
